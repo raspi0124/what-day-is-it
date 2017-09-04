b4099af09a4ba41fdf1303547f09013b4a6e6571
raspi0124 <adores0124@yahoo.co.jp>
2017-09-04 15:11:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
- <?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("l");
 
-
-$n = array(69, 72);
+<?php
+$week = array(
+  'Sun', //0
+  'Mon', //1
+  'Tues', //2
+  'Wednes', //3
+  'Thurs', //4
+  'Fri', //5
+  'Satur'  //6
+);
  
-for($i = 0; $i <= count($n)-1; $i++){
-    if($n[$i] % 2 == 0) {
-        // 偶数の場合の処理
-        echo $n[$i] .’は偶数です<br />’;
-    }else{
-        // 奇数の場合の処理
-        echo $n[$i] .’は奇数です<br />’;
-    }
-}
-?> 
+$weekno = date('w');
+ 
+echo $week[$weekno] . 'day';
+
+if ($weekno = "1")
+  echo "Monday! school......";
+
+?>
+
