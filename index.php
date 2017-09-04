43dce1ef80177cff83c59690aa3cc0711ad1d8a0
raspi0124 <adores0124@yahoo.co.jp>
2017-09-04 15:38:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,30 @@
 <?php
 $weekno = date('w');
 $isbweek = "1";
+$weekjp = array(
+  'Sun', //0
+  'Mon', //1
+  'Tues', //2
+  'Wednes', //3
+  'Thurs', //4
+  'Fri', //5
+  'Satur'  //6
+);
+ 
+
+echo $weekjp[$weekno] . 'day';
+
 
 if( $weekno == 1 and  $isbweek == 1 ){
   echo "Today is Day 1,";
 }else {
-  echo "Error.Prety confusing....";
+  echo "";
 }
 
-if( $weekno == 1 and  $isbweek == 1 ){
+if( $weekno == 2 and  $isbweek == 1 ){
   echo "Today is Day 2,";
 } else {
-  echo "Error.Prety confusing....";
+  echo "";
 }
 
 ?>
