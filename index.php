956360c354dbb41e17eb08a5f39c096de32f9812
raspi0124 <adores0124@yahoo.co.jp>
2017-09-05 14:01:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,46 @@
-
 <?php
 $weekno = date('w');
 $isbweek = "1";
-$weekjp = array(
-  'Sun', //0
-  'Mon', //1
-  'Tues', //2
-  'Wednes', //3
-  'Thurs', //4
-  'Fri', //5
-  'Satur'  //6
-);
- 
-
-echo $weekjp[$weekno] . 'day';
-
 
 if( $weekno == 1 and  $isbweek == 1 ){
   echo "Today is Day 1,";
 }else {
   echo "";
 }
-
 if( $weekno == 2 and  $isbweek == 1 ){
   echo "Today is Day 2,";
 } else {
   echo "";
 }
 
-?>
+if( $weekno == 3 and  $isbweek == 1 ){
+  echo "Today is Day 3,";
+} else {
+  echo "";
+}
+
+if( $weekno == 4 and  $isbweek == 1 ){
+  echo "Today is Day 4,";
+} else {
+  echo "";
+}
+
+if( $weekno == 5 and  $isbweek == 1 ){
+  echo "Today is Day 5,";
+} else {
+  echo "";
+}
+
+
+if( $weekno == 1 and  $isbweek == 2 ){
+  echo "Today is Day 6,";
+} else {
+  echo "";
+}
+
+if( $weekno == 2 and  $isbweek == 2 ){
+  echo "Today is Day 7,";
+} else {
+  echo "";
+}
+
